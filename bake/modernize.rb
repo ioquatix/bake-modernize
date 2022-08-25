95197c2b499d3e4d26a84ef6de8fa49c14fd2514
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-08-25 22:08:10 +1200

    Add support for automatically adding `frozen_string_literal`.

--- bake/modernize.rb
+++ bake/modernize.rb
@@ -4,5 +4,5 @@
 # Copyright, 2020-2022, by Samuel Williams.
 
 def modernize
-	call('modernize:git', 'modernize:readme', 'modernize:actions', 'modernize:editorconfig', 'modernize:gemfile', 'modernize:signing', 'modernize:gemspec', 'modernize:license')
+	call('modernize:git', 'modernize:readme', 'modernize:actions', 'modernize:editorconfig', 'modernize:gemfile', 'modernize:signing', 'modernize:gemspec', 'modernize:license', 'modernize:frozen_string_literal')
 end
---
+++ bake/modernize/frozen_string_literal.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+def frozen_string_literal
+	update(root: Dir.pwd)
+end
+
+def update(root:)
+	Dir.glob("**/*.rb", base: root).each do |path|
+		case path
+		when /\.rb$/
+			update_source_file(path)
+		end
+	end
+end
+
+private
+
+def update_source_file(path)
+	input = File.readlines(path)
+	output = []
+	
+	# Skip the hash-bang line:
+	if input.first =~ /^\#!/
+		output.push input.shift
+	end
+	
+	options = {}
+	
+	while input.first =~ /^\#\s*(.*?):(.*)$/
+		options[$1.strip] = $2.strip
+		input.shift
+	end
+	
+	options['frozen_string_literal'] ||= 'true'
+	
+	options.each do |key, value|
+		output.push "# #{key}: #{value}"
+	end
+	
+	unless input.first.chomp.empty?
+		output.push "\n"
+	end
+	
+	# Add the rest of the file:
+	output.push(*input)
+	
+	File.open(path, "w") do |file|
+		file.puts(output)
+	end
+end
