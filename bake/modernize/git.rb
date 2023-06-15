577a6dcbcee76ee9ebd79855a8d6ff24757bc971
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-06-15 22:33:25 +0900

    Preserve custom modifications to `.gitignore`.

--- bake/modernize/git.rb
+++ bake/modernize/git.rb
@@ -16,11 +16,32 @@ def update(root:)
 		system("git", "push", "-u", "origin", "main")
 	end
 	
+	current_gitignore_custom_lines = self.current_gitignore_custom_lines(root)
+	
 	template_root = Bake::Modernize.template_path_for('git')
 	Bake::Modernize.copy_template(template_root, root)
+	
+	if current_gitignore_custom_lines
+		File.open(File.join(root, ".gitignore"), "a") do |file|
+			file.puts
+			file.puts(current_gitignore_custom_lines)
+		end
+	end
 end
 
 private
+
+def current_gitignore_custom_lines(root)
+	gitignore_path = File.join(root, ".gitignore")
+	
+	if File.exist?(gitignore_path)
+		lines = File.readlines(gitignore_path)
+		if blank_index = lines.index{|line| line =~ /^\s*$/}
+			lines.shift(blank_index+1)
+			return lines
+		end
+	end
+end
 
 def current_branch
 	require 'open3'
