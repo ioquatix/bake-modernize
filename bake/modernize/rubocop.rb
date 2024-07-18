9c8e2827053fa66fdf544a3035324915b0075257
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-07-18 15:33:11 +1200

    Add basic RuboCop code check.

---
+++ bake/modernize/rubocop.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Released under the MIT License.
+# Copyright, 2022, by Samuel Williams.
+
+require 'bake/modernize'
+require 'build/files/system'
+
+def rubocop
+	update(root: Dir.pwd)
+end
+
+def update(root:)
+	system("bundle", "add", "rubocop", "--group", "maintenance", chdir: root)
+	
+	template_root = Bake::Modernize.template_path_for('rubocop')
+	Bake::Modernize.copy_template(template_root, root)
+end
