5d0e6d0e67864af15a52c9815166864c4006795c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-08-05 11:29:47 +1200

    Use project root for gemspec context test

--- test/bake/modernize/gemspec.rb
+++ test/bake/modernize/gemspec.rb
@@ -10,7 +10,7 @@ require "sus/fixtures/temporary_directory_context"
 describe "modernize:gemspec" do
 	include Sus::Fixtures::TemporaryDirectoryContext
 	
-	let(:context) {Bake::Context.load(File.expand_path("../../..", __dir__))}
+	let(:context) {Bake::Context.load(Dir.pwd)}
 	let(:gemspec_path) {File.join(root, "example.gemspec")}
 	let(:output) {StringIO.new}
 	
