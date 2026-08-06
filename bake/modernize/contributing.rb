9bb105f69e50152c095cc7675a099a7095b4b14f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2026-08-06 23:49:38 +1200

    Fix update path too.

--- bake/modernize/contributing.rb
+++ bake/modernize/contributing.rb
@@ -21,11 +21,11 @@ DEFAULT_CONTRIBUTING = <<~EOF
 
 We welcome contributions to this project.
 
-1.  Fork it.
+1.  Fork the repository.
 2.  Create your feature branch (`git checkout -b my-new-feature`).
-3.  Commit your changes (`git commit -am 'Add some feature'`).
+3.  Commit your changes (`git commit -am 'Add some feature.'`).
 4.  Push to the branch (`git push origin my-new-feature`).
-5.  Create new Pull Request.
+5.  Create a new pull request.
 
 ### Running Tests
 
